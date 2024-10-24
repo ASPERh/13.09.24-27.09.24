333f23136fff7ce6be82be5bcfeba60ab61bec31
ASPER <144143767+ASPERh@users.noreply.github.com>
2024-10-24 22:27:19 +0100

    Add files via upload

---
+++ 12.09.24/12.09.24/Group.cs
@@ -0,0 +1,254 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HW.Student
+{
+    class Group : IComparable<Group>, ICloneable, IEnumerable<Student>
+    {
+        private NameGroup namegroup;
+        private int courseNumber;
+        private bool passedSession;
+        public List<Student> students;
+        private Specialization specialization;
+
+        enum NameGroup
+        {
+            P28,
+            P26,
+            P27,
+            P25
+
+        }
+        enum Specialization
+        {
+            Math,
+            ComputerScience,
+            PE,
+            Economy,
+            WebDesign,
+            GameDesign
+        }
+        public Group()
+        {
+            Inizialization();
+        }
+
+        public void Inizialization()
+        {
+            namegroup = GetRandomGroupName();
+            specialization = GetRandomSpecialization();
+            courseNumber = GetRandomCourseNumber();
+
+            students = new List<Student>();
+        }
+        private NameGroup GetRandomGroupName()
+        {
+            Random rand = new Random();
+            Array values = Enum.GetValues(typeof(NameGroup));
+            return (NameGroup)values.GetValue(rand.Next(values.Length));
+        }
+        private Specialization GetRandomSpecialization()
+        {
+            Random rand = new Random();
+            Array values = Enum.GetValues(typeof(Specialization));
+            return (Specialization)values.GetValue(rand.Next(values.Length));
+        }
+
+        private int GetRandomCourseNumber()
+        {
+            Random rand = new Random();
+            return rand.Next(1, 5);
+        }
+        public Group(Group other)
+        {
+            namegroup = other.namegroup;
+            specialization = other.specialization;
+            courseNumber = other.courseNumber;
+            passedSession = other.passedSession;
+            students = new List<Student>();
+        }
+
+
+        public List<Student> GetStudents()
+        {
+            return students;
+        }
+        public void CountMarks()
+        {
+            foreach (var student in students)
+            {
+                double resultmarks = ((student.GetExams().Sum() / 2) + (student.homeworks.Sum() / 13) + (student.courseWorks.Sum() / 4)) / 3;
+
+                if (resultmarks <= 6)
+                {
+                    Console.WriteLine($" {student.surname} {student.name} не набрал нужный бал :((");
+                    passedSession = false;
+                }
+                else
+                {
+                    Console.WriteLine($" {student.surname} {student.name} набрал нужный бал!!!");
+                    passedSession = true;
+                }
+            }
+        }
+        public void ShowAllStudents()
+        {
+            if (students.Count > 0)
+            {
+                Console.WriteLine("Информация о группе:");
+                PrintGroup();
+                Console.WriteLine();
+            }
+            var sortedStudents = students.OrderBy(s => s.surname).ToList();
+
+
+            foreach (var student in sortedStudents)
+            {
+                Console.WriteLine($"Фамилия: {student.surname}");
+                Console.WriteLine($"Имя: {student.name}");
+                Console.WriteLine($"Отчество: {student.papaname}");
+                Console.WriteLine($"Адрес: {student.adress}");
+                Console.WriteLine($"Телефон: {student.number}");
+                Console.Write("Домашние задания: ");
+                foreach (int mark in student.homeworks)
+                {
+                    Console.Write(mark + " ");
+                }
+                Console.WriteLine();
+
+                Console.Write("Курсовые работы: ");
+                foreach (int mark in student.courseWorks)
+                {
+                    Console.Write(mark + " ");
+                }
+                Console.WriteLine();
+
+                Console.Write("Экзамены: ");
+                foreach (int mark in student.exams)
+                {
+                    Console.Write(mark + " ");
+                }
+                Console.WriteLine();
+
+                Console.WriteLine();
+            }
+            Console.WriteLine("Остальная информация:");
+            CountMarks();
+        }
+        public void AddStudent(Student student)
+        {
+            students.Add(student);
+        }
+        public void EditGroup(int newCourseNumber)
+        {
+            courseNumber = newCourseNumber;
+            Console.WriteLine($"Номер курса обновлен на {courseNumber}.");
+        }
+        public void PerevodStudent(Group targetGroup, Student student)
+        {
+            if (students.Contains(student))
+            {
+                students.Remove(student);
+                targetGroup.AddStudent(student);
+            }
+        }
+        public void FailedStudents()
+        {
+            List<Student> failedStudents = new List<Student>();
+
+            foreach (var student in students)
+            {
+                double averageMark = ((student.exams.Sum() / (double)student.exams.Count));
+                if (averageMark < 6)
+                {
+                    failedStudents.Add(student);
+                }
+            }
+            foreach (var failedStudent in failedStudents)
+            {
+                students.Remove(failedStudent);
+                Console.WriteLine($"Студент {failedStudent.surname} {failedStudent.name} {failedStudent.papaname} был удален за низкий средний балл за экзамены.");
+            }
+
+            Console.WriteLine($"Количество отчисленных студентов: {failedStudents.Count}");
+        }
+        public void RemoveWorstStudent()
+        {
+            Student worstStudent = null;
+            double worstAverage = double.MaxValue;
+
+            foreach (var student in students)
+            {
+                double averageMark = ((student.exams.Sum() / (double)student.exams.Count) + (student.homeworks.Sum() / (double)student.homeworks.Count) + (student.courseWorks.Sum() / (double)student.courseWorks.Count)) / 3;
+
+                if (averageMark < worstAverage)
+                {
+                    worstAverage = averageMark;
+                    worstStudent = student;
+                }
+            }
+
+            if (worstStudent != null)
+            {
+                students.Remove(worstStudent);
+                Console.WriteLine($"Студент {worstStudent.surname} {worstStudent.name} был удален из группы за самый низкий средний балл");
+            }
+        }
+        public void PrintGroup()
+        {
+            Console.WriteLine($"Имя группы: {namegroup}");
+            Console.WriteLine($"Специализация: {specialization}");
+            Console.WriteLine($"Номер курса: {courseNumber}");
+        }
+
+        public static bool operator ==(Group group1, Group group2)
+        {
+            return group1.students.Count == group2.students.Count;
+        }
+
+        public static bool operator !=(Group group1, Group group2)
+        {
+            return !(group1 == group2);
+        }
+        public int CompareTo(Group other)
+        {
+            return this.students.Count.CompareTo(other.students.Count);
+        }
+        public object Clone()
+        {
+            Group clonedGroup = new Group
+            {
+                namegroup = this.namegroup,
+                specialization = this.specialization,
+                courseNumber = this.courseNumber,
+                passedSession = this.passedSession,
+                students = new List<Student>()
+            };
+
+            foreach (var student in this.students)
+            {
+                clonedGroup.students.Add((Student)student.Clone());
+            }
+
+            return clonedGroup;
+        }
+        public IEnumerator<Student> GetEnumerator()
+        {
+            return students.GetEnumerator();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+    }
+}
+
+
+
+
+
---
+++ 12.09.24/12.09.24/Program.cs
@@ -0,0 +1,142 @@
+﻿using System;
+
+namespace HW.Student
+{
+    class Program
+    {
+        static void Main()
+        {
+            Group group1 = new Group();
+            Group group2 = new Group();
+            Group group3 = new Group();
+
+            Student Stud1 = new Student("Дима", "Шевелеёв", "Алексеевич", "Новосельского 41", 3807340555555);
+            Student Stud2 = new Student("Илья", "Нестеров", "Александрович", "Новосельского 41", 3807347255555);
+            Student Stud3 = new Student("Данил", "Грачов", "Васильич", "Новосельского 32", 3807348455555);
+            Student Stud4 = new Student("Вадим", "Некрасов", "Анатоливич", "Новосельского 2", 3807348455555);
+            Student Stud5 = new Student("Валентин", "Горбачев", "Виталивич", "Новосельского 12", 3807348455555);
+            Student Stud6 = new Student("Влад", "Радан", "Элденрингович", "Новосельского 61", 3807348455555);
+
+            Stud1.GenericHomeworks();
+            Stud1.GenericCoursWorks();
+            Stud1.GenericExams();
+
+            Stud2.GenericHomeworks();
+            Stud2.GenericCoursWorks();
+            Stud2.GenericExams();
+
+            Stud3.GenericHomeworks();
+            Stud3.GenericCoursWorks();
+            Stud3.GenericExams();
+
+            Stud4.GenericHomeworks();
+            Stud4.GenericCoursWorks();
+            Stud4.GenericExams();
+
+            Stud5.GenericHomeworks();
+            Stud5.GenericCoursWorks();
+            Stud5.GenericExams();
+
+            Stud6.GenericHomeworks();
+            Stud6.GenericCoursWorks();
+            Stud6.GenericExams();
+
+            group1.AddStudent(Stud1);
+            group1.AddStudent(Stud2);
+            group2.AddStudent(Stud3);
+            group2.AddStudent(Stud4);
+            group3.AddStudent(Stud5);
+            group3.AddStudent(Stud6);
+
+            group1.ShowAllStudents();
+            group2.ShowAllStudents();
+            group3.ShowAllStudents();
+
+            Console.WriteLine("Введите новый номер курса для группы 1 (от 1 до 4):");
+            if (int.TryParse(Console.ReadLine(), out int newCourseNumber1) && newCourseNumber1 >= 1 && newCourseNumber1 <= 4)
+            {
+                group1.EditGroup(newCourseNumber1);
+            }
+            else
+            {
+                Console.WriteLine("Некорректный ввод номера курса.");
+            }
+
+            Console.WriteLine("\nОбновленная информация о группе 1:");
+            group1.ShowAllStudents();
+
+            Group newGroup = new Group();
+            group1.PerevodStudent(newGroup, Stud1);
+            group3.PerevodStudent(newGroup, Stud5);
+            newGroup.PrintGroup();
+
+            foreach (var student in group1.GetStudents())
+            {
+                Console.WriteLine($"\n{student.GetSurname()} {student.GetName()} {(student ? "сдал" : "не сдал")}");
+            }
+            foreach (var student in group2.GetStudents())
+            {
+                Console.WriteLine($"\n{student.GetSurname()} {student.GetName()} {(student ? "сдал" : "не сдал")}");
+            }
+            foreach (var student in group3.GetStudents())
+            {
+                Console.WriteLine($"\n {student.GetSurname()} {student.GetName()} {(student ? "сдал" : "не сдал")}");
+            }
+
+            if (Stud5 == Stud4)
+            {
+                Console.WriteLine("\nУ студентов Валентин и Вадим одинаковые номера");
+            }
+            else
+            {
+                Console.WriteLine("\nУ студентов Валентин и Вадим не одинаковые номера");
+            }
+
+            if (Stud1 != Stud2)
+            {
+                Console.WriteLine("\nУ студентов Дима и Илья не одинаковые номера");
+            }
+
+            if (Stud1 > Stud2)
+            {
+                Console.WriteLine("\nДима имеет лучший средний балл, чем Илья");
+            }
+            else if (Stud1 < Stud2)
+            {
+                Console.WriteLine("\nИлья имеет лучший средний балл, чем Дима");
+            }
+            else
+            {
+                Console.WriteLine("\nУ студентов одинаковые средние баллы");
+            }
+
+            if (group1 == group2)
+            {
+                Console.WriteLine("\nГруппы 1 и 2 равны по количеству студентов.");
+            }
+            else
+            {
+                Console.WriteLine("\nГруппы 1 и 2 не равны по количеству студентов.");
+            }
+
+
+
+            Console.WriteLine("\nУдаление студентов с низким средним баллом и самого худшего студента:");
+            group1.FailedStudents();
+            group1.RemoveWorstStudent();
+
+            var sortedByName = group1.students.OrderBy(s => s, new Student.CompareByName());
+            Console.WriteLine("Студенты, отсортированные по имени:");
+            foreach (var student in sortedByName)
+            {
+                Console.WriteLine(student.name);
+            }
+
+            Console.WriteLine("\nСтуденты в группе:");
+            foreach (var student in group1)
+            {
+                Console.WriteLine($"Фамилия: {student.surname}, Имя: {student.name}");
+            }
+        }
+    }
+}
